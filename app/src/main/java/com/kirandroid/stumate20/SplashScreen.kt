8a1f152f7e7d87817099da89a9d34cae68a33ce0
saikiran1224 <knvrssaikiran@gmail.com>
2022-09-03 00:01:37 +0530

    Completed Splash Screen

--- app/src/main/java/com/kirandroid/stumate20/SplashScreen.kt
+++ app/src/main/java/com/kirandroid/stumate20/SplashScreen.kt
@@ -1,30 +1,51 @@
 package com.kirandroid.stumate20
 
+import android.view.animation.OvershootInterpolator
 import android.window.SplashScreen
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.scale
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.res.painterResource
 import androidx.navigation.NavController
+import kotlinx.coroutines.delay
 
 @Composable
 fun SplashScreen(navController: NavController) {
 
-    Box(modifier = Modifier
-        .fillMaxSize()
-        .background(MaterialTheme.colorScheme.primary),
-        contentAlignment = Alignment.Center
-    ) {
+    val scale = remember {
+        androidx.compose.animation.core.Animatable(0f)
+    }
 
-         // TODO Scope of the Box - Display Stumate logo in center here
-        val image: Painter = painterResource(id = R.drawable.splash_logo)
-        Image(painter = image, contentDescription = "Splash Screen Image")
+    // AnimationEffect
+    LaunchedEffect(key1 = true) {
+        scale.animateTo(
+            targetValue = 0.7f,
+            animationSpec = tween(
+                durationMillis = 800,
+                easing = {
+                    OvershootInterpolator(4f).getInterpolation(it)
+                })
+        )
+        delay(3000L)
+        navController.navigate("home_screen")
+    }
+
+    // Image
+    Box(contentAlignment = Alignment.Center,
+        modifier = Modifier.fillMaxSize().background(MaterialTheme.colorScheme.primary)) {
+        Image(painter = painterResource(id = R.drawable.splash_logo),
+            contentDescription = "Logo",
+            modifier = Modifier.scale(scale.value))
     }
 
 }
--- app/src/main/java/com/kirandroid/stumate20/navigation/Navgraph.kt
+++ app/src/main/java/com/kirandroid/stumate20/navigation/Navgraph.kt
@@ -4,11 +4,13 @@ import android.provider.CalendarContract
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.Font
 import androidx.navigation.NavController
 import androidx.navigation.NavHost
 import androidx.navigation.NavHostController
@@ -33,9 +35,9 @@ fun SetupNavGraph(navController: NavHostController) {
         composable(route = Screen.Home.route) {
             Box(modifier = Modifier
                 .fillMaxSize()
-                .background(Color.Blue),
+                .background(MaterialTheme.colorScheme.primary),
             contentAlignment = Alignment.Center) {
-                Text(text = "Welcome to Stumate 2.0!")
+                Text(text = "Welcome to Stumate 2.0!", color = Color.White)
             }
         }
         
